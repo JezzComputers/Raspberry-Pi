4b5015139ca9d9eb9a95047c72f2215a14fdd9da
Jeremy <jeremy_sprague@outlook.com>
2026-01-18 08:32:50 +1100

    Delete Sense-Hat directory

--- Sense-Hat/Compass.py
+++
@@ -1,9 +0,0 @@
-# Replace sence_emu with sence_hat for use with physical HAT
-
-from sense_emu import SenseHat
-sense = SenseHat()
-sense.clear()
-        
-orientation = sense.get_gyroscope_raw()
-north = sense.get_compass()
-print(north)
--- Sense-Hat/Set_Pixels.py
+++
@@ -1,7 +0,0 @@
-# Replace sence_emu with sence_hat for use with physical HAT
-
-from sense_emu import SenseHat
-sense = SenseHat()
-sense.clear()
-
-sense.set_pixel(1, 1, (0, 255, 0))
