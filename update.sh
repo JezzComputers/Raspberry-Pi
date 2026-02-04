89b4e5c07d0a2f0042fd56c772f4badfbbb67976
Jeremy <jeremy_sprague@outlook.com>
2026-02-05 08:11:08 +1100

    Improve update.sh for multi-distro support
    Refactor update script to handle different Linux distributions.

--- update.sh
+++ update.sh
@@ -1,1 +1,17 @@
-sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean && sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean
+#!/usr/bin/env bash
+
+case $(awk -F= '/^ID=/{gsub(/"/, "", $2); print $2}' /etc/os-release) in
+	debian|ubuntu|kali|raspbian)
+		sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean
+		;;
+	arch|manjaro|endeavouros)
+		if command -v yay &> /dev/null; then
+			yay -Syu --noconfirm --answerclean None --answerdiff None
+		else
+			sudo pacman -Syu --noconfirm
+		fi
+		;;
+	*)
+		echo "Unrecognised distro"
+		;;
+esac
